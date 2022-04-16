8b5b47ed0aac81590774790831e921afdce907b1
Habibur137 <infernohabib@gmail.com>
2022-04-17 00:48:55 +0600

    router setup done

---
+++ src/Pages/Header/Header.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import CustomLink from "../../CustomLink/CustomLink";
+import logo from "../../images/trawell_logo_mini.png";
+
+const Header = () => {
+  return (
+    <div>
+      <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+        <Container>
+          <Navbar.Brand as={Link} to="#home">
+            <img src={logo} alt="" />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ms-auto">
+              <CustomLink
+                className="me-2 text-decoration-none text-dark pb-1"
+                to="/"
+              >
+                Home
+              </CustomLink>
+              <CustomLink
+                className="me-2 text-decoration-none text-dark pb-1"
+                to="/services"
+              >
+                Services
+              </CustomLink>
+              <CustomLink
+                className="me-2 text-decoration-none text-dark pb-1"
+                to="/login"
+              >
+                Sign In
+              </CustomLink>
+              <CustomLink
+                className="pb-1 text-decoration-none text-dark"
+                to="/register"
+              >
+                Sign Up
+              </CustomLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default Header;
