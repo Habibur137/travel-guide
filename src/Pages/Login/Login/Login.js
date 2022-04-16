f79a21ec4d3f066ef8530fe7bc6be9dd96d3aa68
Habibur137 <infernohabib@gmail.com>
2022-04-17 05:38:55 +0600

    login and registration page designed

--- src/Pages/Login/Login/Login.js
+++ src/Pages/Login/Login/Login.js
@@ -1,9 +1,40 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../images/trawell_logo_mini.png";
 
 const Login = () => {
   return (
-    <div>
-      <h1>this is login</h1>
+    <div className="w-25 border-1 mx-auto container  text-center mt-4  p-4 shadow-lg">
+      <div>
+        <img className="mb-3" src={logo} alt="" />
+        <form className="d-flex flex-column ">
+          <input
+            className="mb-3 bg-light p-2 border-0"
+            type="email"
+            name="email"
+            placeholder="Email"
+          />
+          <input
+            className="mb-3 bg-light p-2 border-0"
+            type="password"
+            name="password"
+            id=""
+            placeholder="Password"
+          />
+          <button className="mb-3 bg-info p-2 border-0 text-white">
+            Sign In
+          </button>
+        </form>
+        <p style={{ color: "gray" }}>
+          Need An Account ?{" "}
+          <Link className="text-info" to="/register">
+            Sign Up
+          </Link>
+        </p>
+        <p style={{ cursor: "pointer" }} className="text-info">
+          Forgot Password?
+        </p>
+      </div>
     </div>
   );
 };
--- src/Pages/Login/Register/Register.js
+++ src/Pages/Login/Register/Register.js
@@ -1,9 +1,45 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logo from "../../../images/trawell_logo_mini.png";
 
 const Register = () => {
   return (
     <div>
-      <h1>this is register</h1>
+      <div className="w-25 border-1 mx-auto container  text-center mt-4  p-4 shadow-lg">
+        <div>
+          <img className="mb-3" src={logo} alt="" />
+          <form className="d-flex flex-column ">
+            <input
+              className="mb-3 bg-light p-2 border-0"
+              type="text"
+              name="name"
+              placeholder="Name"
+            />
+            <input
+              className="mb-3 bg-light p-2 border-0"
+              type="email"
+              name="email"
+              placeholder="Email"
+            />
+            <input
+              className="mb-3 bg-light p-2 border-0"
+              type="password"
+              name="password"
+              id=""
+              placeholder="Password"
+            />
+            <button className="mb-3 bg-info p-2 border-0 text-white">
+              Sign In
+            </button>
+          </form>
+          <p style={{ color: "gray" }}>
+            Already Have An Account ?{" "}
+            <Link className="text-info" to="/login">
+              Sign In
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
